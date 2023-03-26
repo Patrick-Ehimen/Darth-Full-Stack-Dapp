eb670c05c75e763cedb86c846669ff50b1a340d7
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-26 12:10:30 +0100

    Update NFT_Staking.sol

--- src/NFT_Staking.sol
+++ src/NFT_Staking.sol
@@ -1,18 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
 
-import "./Darth_ERC20Token.sol";
-import "./Darth_NFT.sol";
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract NFT_Staking is Darth_NFT, Darth_ERC20Token {
-    bool public active;
-    uint256 public startTime;
-    uint256 public cutoffTime;
-
-    struct rewardSchedule {
-        uint64 days30;
-        uint64 days45;
-        uint64 days60;
-        uint64 days90;
-    }
-}
+contract DarthStaking {}
